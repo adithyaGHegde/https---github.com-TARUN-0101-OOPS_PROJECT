505bae7a638b5290210ac91e4302a9921ca68abd
TARUN-0101 <82829159+TARUN-0101@users.noreply.github.com>
2021-11-07 11:46:10 +0530

    FIRST RESP

---
+++ app/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.views import View
+from .models import OrderPlaced, customer, Product, Cart, OrderPlaced
+
+#def home(request):
+ #return render(request, 'app/home.html')
+
+class ProductView(View):
+    def get(self, request):
+        COFFEE = Product.objects.filter(category='C')
+        TEA = Product.objects.filter(category='T')
+        SNACKS = Product.objects.filter(category='S')
+        BRUNCH = Product.objects.filter(category='B')
+        return render(request, 'app/home.html', {'COFFEE':COFFEE, 'TEA':TEA, 'SNACKS':SNACKS, 'BRUNCH':BRUNCH})
+
+def product_detail(request):
+ return render(request, 'app/productdetail.html')
+
+def add_to_cart(request):
+ return render(request, 'app/addtocart.html')
+
+def buy_now(request):
+ return render(request, 'app/buynow.html')
+
+def profile(request):
+ return render(request, 'app/profile.html')
+
+def address(request):
+ return render(request, 'app/address.html')
+
+def orders(request):
+ return render(request, 'app/orders.html')
+
+def change_password(request):
+ return render(request, 'app/changepassword.html')
+
+def login(request):
+ return render(request, 'app/login.html')
+
+def customerregistration(request):
+ return render(request, 'app/customerregistration.html')
+
+def checkout(request):
+ return render(request, 'app/checkout.html')
